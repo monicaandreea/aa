b7c11758ef0ecb46360e0c244e09cefd44f89f5a
Gîrbea Monica <79595825+monicaandreea@users.noreply.github.com>
2022-04-12 20:18:39 +0300

    Create 1_test_orientare.cpp

---
+++ Lab5/1_test_orientare.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+/* TEST DE ORIENTARE
+ *            |  1  1  1 |   { <0, R in dreapta lui PQ
+ * d(P,Q,R) = | xp xq xr | = { 0, coliniare
+ *            | yp yq yr |   { >0, R in stanga lui PQ
+ */
+
+struct Punct{
+    int x;
+    int y;
+};
+
+int testOrientare(Punct P, Punct Q, Punct R){
+    int d = (Q.x * R.y) + (R.x * P.y) + (P.x * Q.y) - (Q.x * P.y) - (R.x * Q.y) - (P.x * R.y);
+
+    if(d < 0) return 1;
+    else if(d == 0) return 2;
+    else return 3;
+}
+
+int main() {
+    int x, y;
+    std::cout<<"P:";
+    std::cin>>x>>y;
+    Punct P = {x, y};
+    std::cout<<"Q:";
+    std::cin>>x>>y;
+    Punct Q = {x, y};
+    std::cout<<"R:";
+    std::cin>>x>>y;
+    Punct R = {x, y};
+
+    int rezultat = testOrientare(P, Q, R);
+
+    if(rezultat == 1) std::cout<<"Viraj la dreapta.";
+    else if(rezultat == 2) std::cout<<"Puncte coliniare";
+    else std::cout<<"Viraj la stanga.";
+
+    return 0;
+}
