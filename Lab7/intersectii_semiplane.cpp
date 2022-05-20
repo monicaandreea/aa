dc647a9b557bdce390d8337864c20b184c683d1a
Gîrbea Monica <79595825+monicaandreea@users.noreply.github.com>
2022-05-20 16:09:51 +0300

    Create intersectii_semiplane.cpp

---
+++ Lab7/intersectii_semiplane.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#define INF 10000000
+
+int main(){
+    int n, a,b,c;
+    int N = INF;
+    int S = -INF;
+    int E = INF;
+    int V = -INF;
+
+    std::cin>>n;
+
+    while(n--){
+        std::cin>>a>>b>>c;
+        if(b<0){
+            if(c>S) S=(-c/b);
+        }
+        if(b>0){
+            if(c<N) N=(-c/b);
+        }
+        if(a<0){
+            if(c>V) V=(-c/a);
+        }
+        if(a>0){
+            if(c<E) E=(-c/a);
+        }
+    }
+
+    //std::cout<<S<<" "<<N<<" "<<E<<" "<<V;
+
+    if (N>S && V<E && S!=-INF && N!=INF && V!=-INF && E!=INF) std::cout<<"BOUNDED";
+    else if(N<S || V>E) std::cout<<"VOID";
+    else std::cout<<"UNBOUNDED";
+
+    return 0;
+}
